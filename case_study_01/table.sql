a0e4269c5bf0dd48718af72044122f8122f53df7
Nguyễn Thanh Lâm <wanerbros9@gmail.com>
2021-07-22 12:08:16 +0700

    initial commit

--- case_study_01/table.sql
+++ case_study_01/table.sql
@@ -48,7 +48,7 @@ CREATE TABLE khach_hang(
     ngay_sinh DATE,
     so_CMTND VARCHAR(45),
     sdt VARCHAR(45),
-    emai VARCHAR(45),
+    email VARCHAR(45),
     dia_chi VARCHAR(45),
     FOREIGN KEY (id_loai_khach) REFERENCES loai_khach(id_loai_khach)
 );
@@ -108,3 +108,54 @@ CREATE TABLE hop_dong_chi_tiet(
     FOREIGN KEY (id_hop_dong) REFERENCES hop_dong(id_hop_dong),
     FOREIGN KEY (id_dich_vu_di_kem) REFERENCES dich_vu_di_kem(id_dich_vu_di_kem)
 );
+
+INSERT INTO vi_tri(ten_vi_tri)
+VALUES ('Lễ tân'),
+		('Phục vụ'),
+        ('Chuyên viên'),
+        ('Giám sát'),
+        ('Quản lý'),
+        ('Giám đốc');
+        
+INSERT INTO trinh_do(trinh_do)
+VALUES ('Trung cấp'),
+		('Cao đẳng'),
+		('Đại học'),
+		('Sau đại học');
+        
+INSERT INTO bo_phan(ten_bo_phan)
+VALUES ('Sale-Marketing'),
+		('Hành chính'),
+		('Phục vụ'),
+		('Quản lý');
+
+INSERT INTO nhan_vien(ho_ten, id_vi_tri, id_trinh_do, id_bo_phan, ngay_sinh,so_CMTND, luong, sdt, email, dia_chi)
+VALUES ('Lâm', 2, 3, 3, '1998-10-16', 201757954, 10000, 0825161098, 'lam@gmail.com', 'Đà Nẵng'),
+		('Khoa', 1, 1, 3, '2000-05-11', 201751546, 5000, 0917657904, 'khoa@gmail.com', 'Huế'),
+		('Tùng', 5, 4, 4, '1992-03-18', 201718628, 35000, 0818467035, 'tung@gmail.com', 'Vinh'),
+		('Huy', 6, 4, 2, '1991-12-12', 201792127, 50000, 0818467035, 'thanh@gmail.com', 'Quảng Trị');
+
+INSERT INTO loai_khach(ten_loai_khach)
+VALUES ('Diamond'),
+		('Platinium'),
+        ('Gold'),
+        ('Silver'),
+        ('Member');
+
+INSERT INTO khach_hang(id_loai_khach, ho_ten, ngay_sinh, so_CMTND, sdt, email, dia_chi)
+VALUES (5, 'Minh', '2000-10-06', 201562744, 0843670087, 'minh@gmail.com', 'Đà Nẵng'),
+		(1, 'Danh', '1997-08-22', 201684297, 0394581627, 'danh@gmail.com', 'Huế'),
+		(3, 'Toan', '1994-11-11', 201251983, 0871555364, 'toan@gmail.com', 'Vinh'),
+		(2, 'Uyen', '1996-01-28', 201523799, 0177646469, 'uyen@gmail.com', 'Quảng Trị'),
+		(4, 'Hien', '1997-07-17', 201588114, 0111346999, 'hien@gmail.com', 'Đà Nẵng');
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
