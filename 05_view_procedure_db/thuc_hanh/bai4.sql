38ea0c59ecfd2c5a0f37eb1737e7ef0d10f9e41c
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-07-26 21:41:29 +0700

    initial commit

---
+++ 05_view_procedure_db/thuc_hanh/bai4.sql
@@ -0,0 +1,19 @@
+CREATE VIEW customer_views AS
+
+SELECT customerNumber, customerName, phone
+
+FROM  customers;
+
+select * from customer_views;
+
+CREATE OR REPLACE VIEW customer_views AS
+
+SELECT customerNumber, customerName, contactFirstName, contactLastName, phone
+
+FROM customers
+
+WHERE city = 'Nantes';
+
+DROP VIEW view_name;
+
+DROP VIEW customer_views;
---
+++ case_study_01/case_study_21_28.sql
@@ -0,0 +1,7 @@
+-- 21. Tạo khung nhìn có tên là V_NHANVIEN để lấy được thông tin của tất cả
+-- các nhân viên có địa chỉ là “Hải Châu” và đã từng lập hợp đồng cho 1
+-- hoặc nhiều Khách hàng bất kỳ với ngày lập hợp đồng là “12/12/2019”
+
+-- 22. Thông qua khung nhìn V_NHANVIEN thực hiện cập nhật địa chỉ thành
+-- “Liên Chiểu” đối với tất cả các Nhân viên được nhìn thấy bởi khung nhìn
+-- này.
