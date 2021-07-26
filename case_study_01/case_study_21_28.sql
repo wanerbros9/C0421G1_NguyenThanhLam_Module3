79862554be43d12f7d61c92a8a5b1c65fb3e8b32
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-07-26 23:21:13 +0700

    initial commit

--- case_study_01/case_study_21_28.sql
+++ case_study_01/case_study_21_28.sql
@@ -21,6 +21,51 @@ SET SQL_SAFE_UPDATES = 0;
 
 -- 23. Tạo Store procedure Sp_1 Dùng để xóa thông tin của một Khách hàng
 -- nào đó với Id Khách hàng được truyền vào như là 1 tham số của Sp_1
+delimiter //
+CREATE PROCEDURE sp_1 (
+id_delete INT
+)
+BEGIN
+DELETE 
+FROM khach_hang kh
+WHERE id_khach_hang = id_delete;
+END;
+// delimiter ;
+
+CALL sp_1(1);
+
+-- 24. Tạo Store procedure Sp_2 Dùng để thêm mới vào bảng HopDong với yêu
+-- cầu Sp_2 phải thực hiện kiểm tra tính hợp lệ của dữ liệu bổ sung, với
+-- nguyên tắc không được trùng khóa chính và đảm bảo toàn vẹn tham
+-- chiếu đến các bảng liên quan.
+DELIMITER //
+CREATE PROCEDURE sp_2(
+id_nhan_vien_a INT,
+id_khach_hang_a INT,
+id_dich_vu_a INT,
+ngay_lam_hop_dong_a DATE,
+ngay_ket_thuc_a DATE,
+tien_dat_coc_a INT 
+)
+begin
+if (id_nhan_vien_a in (select id_nhan_vien from nhan_vien) AND 
+	id_khach_hang_a in (SELECT id_khach_hang FROM khach_hang)AND 
+    id_dich_vu_a IN (SELECT id_dich_vu FROM dich_vu)) THEN 
+    INSERT INTO hop_dong(id_nhan_vien, id_khach_hang, id_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc)
+    VALUES (id_nhan_vien_a, id_khach_hang_a, id_dich_vu_a, ngay_lam_hop_dong_a, ngay_ket_thuc_a, tien_dat_coc_a);
+    else 
+    select 'sai r';
+    end if;
+end;
+// DELIMITER ;
+
+CALL sp_2(1, 1, 1, '2021-07-14', '2021-08-11', 50000);
+
+
+
+
+
+
 
 
 
